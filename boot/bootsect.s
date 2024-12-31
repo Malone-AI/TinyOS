3fa511368d1bd932e60a47abfa4f990e65546322
Malone <favorite.vampire@qq.com>
2025-01-01 00:27:44 +0800

    Initialize segment register and stack

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,8 +1,14 @@
 mov ax,0x07c0
-mov ds,ax ; ds <- 0x07c0
+mov ds,ax   ; ds <- 0x07c0
 mov ax,0x9000
-mov es,ax ; es <- 0x9000
+mov es,ax   ; es <- 0x9000
 mov cx,#256
-xor si,si ; si <- 0
-xor di,di ; di <- 0
-rep movw  ; es:di <- ds:si, repeat 256 times
+xor si,si   ; si <- 0
+xor di,di   ; di <- 0
+rep movw    ; es:di <- ds:si, repeat 256 times
+jmpi go,0x9000  ; ip <- segment go, cs <- 0x9000
+mov ax,cs
+mov ds,ax   ; ds <- 0x9000
+mov es,ax   ; es <- 0x9000
+mov ss,ax   ; ss <- 0x9000
+mov sp,#0xFF00  ; sp <- 0x9000, the stack address ss:sp is 0x9000 * 0x0010 + 0xFF00 = 0x9FF00
