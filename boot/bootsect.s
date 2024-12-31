b90892f906f292abcab8e530a999277b6efa5729
Malone <favorite.vampire@qq.com>
2025-01-01 00:19:35 +0800

    Add some comments

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,8 +1,8 @@
 mov ax,0x07c0
-mov ds,ax
+mov ds,ax ; ds <- 0x07c0
 mov ax,0x9000
-mov es,ax
+mov es,ax ; es <- 0x9000
 mov cx,#256
-xor si,si
-xor di,di
-rep movw
+xor si,si ; si <- 0
+xor di,di ; di <- 0
+rep movw  ; es:di <- ds:si, repeat 256 times
